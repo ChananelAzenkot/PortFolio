1f71d0ce170d167b043dd9a806e50aeda0084965
chananelaze <azenkot.chananel@gmail.com>
2023-07-11 00:57:41 +0300

    PageForProjects

--- scripts/script.js
+++ scripts/script.js
@@ -17,4 +17,5 @@ sr.reveal(".hero-text", { delay: 200, origin: "top" });
 sr.reveal(".hero-img", { delay: 450, origin: "top" });
 sr.reveal(".icons", { delay: 500, origin: "left" });
 sr.reveal(".scroll-down", { delay: 500, origin: "right" });
+
 //
